cc8fabba42f0bef243663c5598bf8c1b240509bd
Michal Lulco <lulco@efabrica.sk>
2016-09-26 08:53:08 +0200

    Removed unused test for pgsql table info

--- tests/Database/Adapter/AdapterTest.php
+++ tests/Database/Adapter/AdapterTest.php
@@ -20,30 +20,20 @@ class AdapterTest extends PHPUnit_Framework_TestCase
         $this->setExpectedException('\LogicException', 'Not yet implemented');
         $adapter->tableInfo('table');
     }
-    
-    public function testPgsqlAdapter()
-    {
-        $pdo = new FakePdo();
-        $adapter = new PgsqlAdapter($pdo);
-        $this->assertInstanceOf('\Phoenix\Database\QueryBuilder\QueryBuilderInterface', $adapter->getQueryBuilder());
 
-        $this->setExpectedException('\LogicException', 'Not yet implemented');
-        $adapter->tableInfo('table');
-    }
-    
     public function testSqliteAdapter()
     {
         $pdo = new PDO('sqlite::memory:');
         $adapter = new SqliteAdapter($pdo);
         $this->assertInstanceOf('\Phoenix\Database\QueryBuilder\QueryBuilderInterface', $adapter->getQueryBuilder());
-        
+
         $pdo->query('CREATE TABLE "test" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL,"title" varchar(255) NOT NULL);');
-        
+
         $tableInfo = $adapter->tableInfo('test');
         $this->assertCount(2, $tableInfo);
         $this->assertArrayHasKey('id', $tableInfo);
         $this->assertArrayHasKey('title', $tableInfo);
-        
+
         foreach ($tableInfo as $column) {
             $this->assertInstanceOf('\Phoenix\Database\Element\Column', $column);
         }
