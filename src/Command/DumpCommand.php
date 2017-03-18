ae3c9e7f9238006dc5763753993004b19661206b
Michal Lulco <lulco@efabrica.sk>
2017-03-18 11:28:42 +0100

    Added options and their implementations to dump command: data, ignore-tables, ignore-data-tables, indent

--- src/Command/DumpCommand.php
+++ src/Command/DumpCommand.php
@@ -3,9 +3,8 @@
 namespace Phoenix\Command;
 
 use Phoenix\Command\AbstractCommand;
-use Phoenix\Migration\Manager;
-use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class DumpCommand extends AbstractCommand
@@ -13,18 +12,25 @@ class DumpCommand extends AbstractCommand
     protected function configure()
     {
         $this->setName('dump')
-            ->setDescription('Dump actual database structure to migration file');
+            ->setDescription('Dump actual database structure to migration file')
+            ->addOption('data', 'd', InputOption::VALUE_NONE, 'Dump structure and also data')
+            ->addOption('ignore-tables', null, InputOption::VALUE_OPTIONAL, 'Comma separaterd list of tables to ignore (Structure and data). Default: phoenix_log')
+            ->addOption('ignore-data-tables', null, InputOption::VALUE_OPTIONAL, 'Comma separaterd list of tables which will be exported without data (Option -d, --data is required to use this option)')
+            ->addOption('indent', 'i', InputOption::VALUE_OPTIONAL, 'Indentation. Available values: 2spaces, 3spaces, 4spaces, 5spaces, tab', '4spaces')
+        ;
+
         parent::configure();
     }
 
     protected function runCommand(InputInterface $input, OutputInterface $output)
     {
+        $ignoredTables = array_map('trim', explode(',', $input->getOption('ignore-tables') . ',' . $this->config->getLogTableName() ? : $this->config->getLogTableName()));
         $output->writeln('');
 
-        $indent = '    ';
+        $indent = $this->getIndent($input);
 
         $migration = '';
-        $tables = $this->getOrderedTables();
+        $tables = $this->getOrderedTables($ignoredTables);
         foreach ($tables as $table) {
             $migration .= $indent . "\$this->table('{$table->getName()}')\n";
             foreach ($table->getColumns() as $column) {
@@ -43,17 +49,42 @@ class DumpCommand extends AbstractCommand
             $migration .= "$indent$indent" . "->create();\n\n";
         }
 
+        if ($input->getOption('data')) {
+            $ignoredDataTables = $input->getOption('ignore-data-tables')
+                ? array_merge($ignoredTables, array_map('trim', explode(',', $input->getOption('ignore-data-tables'))))
+                : $ignoredTables;
+
+            foreach ($tables as $table) {
+                if (in_array($table->getName(), $ignoredDataTables)) {
+                    continue;
+                }
+                $rows = $this->adapter->fetchAll($table->getName());
+                if (empty($rows)) {
+                    continue;
+                }
+                $migration .= "$indent\$this->insert('{$table->getName()}', [\n";
+                foreach ($rows as $row) {
+                    $migration .= "$indent$indent" . "[\n";
+                    foreach ($row as $column => $value) {
+                        $migration .= "$indent$indent$indent'$column' => '" . addslashes($value) . "',\n";
+                    }
+                    $migration .= "$indent$indent],\n";
+                }
+                $migration .= "$indent]);\n\n";
+            }
+        }
+
         $output->write($migration);
 
         $output->writeln('');
     }
 
-    private function getOrderedTables()
+    private function getOrderedTables(array $ignoredTables = [])
     {
         $tables = [];
         $structure = $this->adapter->getStructure();
         foreach ($structure->getTables() as $table) {
-            if ($table->getName() == $this->config->getLogTableName()) {
+            if (in_array($table->getName(), $ignoredTables)) {
                 continue;
             }
             $tables[] = $table;
@@ -68,5 +99,23 @@ class DumpCommand extends AbstractCommand
         }, $columns);
         $implodedColumns = implode(', ', $columns);
         return count($columns) > 1 ? '[' . $implodedColumns . ']' : $implodedColumns;
+    }
+
+    private function getIndent(InputInterface $input)
+    {
+        $indent = strtolower(str_replace([' ', '-', '_'], '', $input->getOption('indent')));
+        if ($indent == '2spaces') {
+            return '  ';
+        }
+        if ($indent == '3spaces') {
+            return '   ';
+        }
+        if ($indent == '5spaces') {
+            return '     ';
+        }
+        if ($indent == 'tab') {
+            return "\t";
+        }
+        return '    ';
     }
 }
