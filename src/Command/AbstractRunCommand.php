f044071fceb67a045ab15ae6540764a03459c627
Michal Lulco <lulco@efabrica.sk>
2017-04-08 23:37:27 +0200

    Changed abstract run - used method from abstract command

--- src/Command/AbstractRunCommand.php
+++ src/Command/AbstractRunCommand.php
@@ -17,8 +17,7 @@ abstract class AbstractRunCommand extends AbstractCommand
     {
         parent::configure();
         $this
-            ->addOption('dry', null, InputOption::VALUE_NONE, 'Only print queries, no execution')
-            ->addOption('output-format', 'f', InputOption::VALUE_REQUIRED, 'Format of the output. Available values: text, json', 'text');
+            ->addOption('dry', null, InputOption::VALUE_NONE, 'Only print queries, no execution');
     }
 
     protected function runCommand(InputInterface $input, OutputInterface $output)
@@ -27,28 +26,26 @@ abstract class AbstractRunCommand extends AbstractCommand
         if ($dry) {
             $output->setVerbosity(OutputInterface::VERBOSITY_DEBUG);
         }
-        $optionsNormal = $input->getOption('output-format') === 'json' ? -1 : 0;
-        $optionsDebug = $input->getOption('output-format') === 'json' ? -1 : OutputInterface::VERBOSITY_DEBUG;
 
         $migrations = $this->findMigrations($input);
         if (empty($migrations)) {
-            $output->writeln('', $optionsNormal);
-            $output->writeln('<info>' . $this->noMigrationsFoundMessage . '</info>', $optionsNormal);
+            $this->writeln('');
+            $this->writeln('<info>' . $this->noMigrationsFoundMessage . '</info>');
         }
 
         $executedMigrations = [];
         foreach ($migrations as $migration) {
-            $output->writeln('', $optionsNormal);
-            $output->writeln('<info>' . $this->migrationInfoPrefix . ' ' . $migration->getClassName() . ' executing</info>', $optionsNormal);
+            $this->writeln('');
+            $this->writeln('<info>' . $this->migrationInfoPrefix . ' ' . $migration->getClassName() . ' executing</info>');
 
             $start = microtime(true);
             $this->runMigration($migration, $dry);
             $executionTime = microtime(true) - $start;
-            $output->writeln('<info>' . $this->migrationInfoPrefix . ' ' . $migration->getClassName() . ' executed</info>. <comment>Took ' . sprintf('%.4fs', $executionTime) . '</comment>', $optionsNormal);
+            $this->writeln('<info>' . $this->migrationInfoPrefix . ' ' . $migration->getClassName() . ' executed</info>. <comment>Took ' . sprintf('%.4fs', $executionTime) . '</comment>');
 
             $executedQueries = $migration->getExecutedQueries();
-            $output->writeln('Executed queries:', $optionsDebug);
-            $output->writeln($executedQueries, $optionsDebug);
+            $this->writeln('Executed queries:', OutputInterface::VERBOSITY_DEBUG);
+            $this->writeln($executedQueries, OutputInterface::VERBOSITY_DEBUG);
 
             $executedMigration = [
                 'classname' => $migration->getClassName(),
@@ -59,21 +56,10 @@ abstract class AbstractRunCommand extends AbstractCommand
             }
             $executedMigrations[] = $executedMigration;
         }
-
-        if ($input->getOption('output-format') === 'json') {
-            $output->write(json_encode(['executed_migrations' => $executedMigrations]));
-        }
+        $this->outputData['executed_migrations'] = $executedMigrations;
     }
 
     abstract protected function findMigrations(InputInterface $input);
 
     abstract protected function runMigration(AbstractMigration $migration, $dry = false);
-
-    protected function finishCommand(InputInterface $input, OutputInterface $output)
-    {
-        if ($input->getOption('output-format') === 'json') {
-            return;
-        }
-        parent::finishCommand($input, $output);
-    }
 }
