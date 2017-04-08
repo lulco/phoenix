982228fdf3061e79a82bfb6d97c0aa95234ca4f4
Michal Lulco <lulco@efabrica.sk>
2017-04-08 16:42:00 +0200

    Added json output for init command

--- src/Command/InitCommand.php
+++ src/Command/InitCommand.php
@@ -5,6 +5,7 @@ namespace Phoenix\Command;
 use Phoenix\Command\AbstractCommand;
 use Phoenix\Migration\Init\Init;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class InitCommand extends AbstractCommand
@@ -12,7 +13,8 @@ class InitCommand extends AbstractCommand
     protected function configure()
     {
         $this->setName('init')
-            ->setDescription('Initialize phoenix');
+            ->setDescription('Initialize phoenix')
+            ->addOption('output-format', 'f', InputOption::VALUE_REQUIRED, 'Format of the output. Available values: text, json', 'text');
 
         parent::configure();
     }
@@ -24,9 +26,32 @@ class InitCommand extends AbstractCommand
         $migration = new Init($this->adapter, $this->config->getLogTableName());
         $migration->migrate();
 
-        $output->writeln('');
-        $output->writeln('<info>Phoenix initialized</info>');
-        $output->writeln('Executed queries:', OutputInterface::VERBOSITY_DEBUG);
-        $output->writeln($migration->getExecutedQueries(), OutputInterface::VERBOSITY_DEBUG);
+        $optionsNormal = $input->getOption('output-format') === 'json' ? -1 : 0;
+        $optionsDebug = $input->getOption('output-format') === 'json' ? -1 : OutputInterface::VERBOSITY_DEBUG;
+
+        $output->writeln('', $optionsNormal);
+        $executedQueries = $migration->getExecutedQueries();
+        $output->writeln('<info>Phoenix initialized</info>', $optionsNormal);
+        $output->writeln('Executed queries:', $optionsDebug);
+        $output->writeln($executedQueries, $optionsDebug);
+
+        $outputData = [
+            'message' => 'Phoenix initialized',
+        ];
+        if ($output->getVerbosity() === OutputInterface::VERBOSITY_DEBUG) {
+            $outputData['executed_queries'] = $executedQueries;
+        }
+
+        if ($input->getOption('output-format') === 'json') {
+            $output->write(json_encode($outputData));
+        }
+    }
+
+    protected function finishCommand(InputInterface $input, OutputInterface $output)
+    {
+        if ($input->getOption('output-format') === 'json') {
+            return;
+        }
+        parent::finishCommand($input, $output);
     }
 }
