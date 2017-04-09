cdadded30eed292adb35a4d2871102f5d3c66493
Michal Lulco <lulco@efabrica.sk>
2017-04-09 14:09:23 +0200

    Rewritten init command to use command's writeln method and added tests

--- src/Command/InitCommand.php
+++ src/Command/InitCommand.php
@@ -5,7 +5,6 @@ namespace Phoenix\Command;
 use Phoenix\Command\AbstractCommand;
 use Phoenix\Migration\Init\Init;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class InitCommand extends AbstractCommand
@@ -13,9 +12,7 @@ class InitCommand extends AbstractCommand
     protected function configure()
     {
         $this->setName('init')
-            ->setDescription('Initialize phoenix')
-            ->addOption('output-format', 'f', InputOption::VALUE_REQUIRED, 'Format of the output. Available values: text, json', 'text');
-
+            ->setDescription('Initialize phoenix');
         parent::configure();
     }
 
@@ -26,32 +23,16 @@ class InitCommand extends AbstractCommand
         $migration = new Init($this->adapter, $this->config->getLogTableName());
         $migration->migrate();
 
-        $optionsNormal = $input->getOption('output-format') === 'json' ? -1 : 0;
-        $optionsDebug = $input->getOption('output-format') === 'json' ? -1 : OutputInterface::VERBOSITY_DEBUG;
-
-        $output->writeln('', $optionsNormal);
+        $this->writeln('');
         $executedQueries = $migration->getExecutedQueries();
-        $output->writeln('<info>Phoenix initialized</info>', $optionsNormal);
-        $output->writeln('Executed queries:', $optionsDebug);
-        $output->writeln($executedQueries, $optionsDebug);
+        $this->writeln('<info>Phoenix initialized</info>');
+        $this->writeln('Executed queries:', OutputInterface::VERBOSITY_DEBUG);
+        $this->writeln($executedQueries, OutputInterface::VERBOSITY_DEBUG);
 
-        $outputData = [
-            'message' => 'Phoenix initialized',
-        ];
+        $this->outputData['message'] = 'Phoenix initialized';
+
         if ($output->getVerbosity() === OutputInterface::VERBOSITY_DEBUG) {
-            $outputData['executed_queries'] = $executedQueries;
+            $this->outputData['executed_queries'] = $executedQueries;
         }
-
-        if ($input->getOption('output-format') === 'json') {
-            $output->write(json_encode($outputData));
-        }
-    }
-
-    protected function finishCommand(InputInterface $input, OutputInterface $output)
-    {
-        if ($input->getOption('output-format') === 'json') {
-            return;
-        }
-        parent::finishCommand($input, $output);
     }
 }
