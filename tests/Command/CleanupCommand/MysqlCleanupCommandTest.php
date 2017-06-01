eb6a8e75df598ebc30cb5a687df4ca338f0a521b
Michal Lulco <lulco@efabrica.sk>
2017-06-01 08:06:17 +0200

    Added tests for CleanupCommand - fixed json output for verbosity debug and added message for json output

---
+++ tests/Command/CleanupCommand/MysqlCleanupCommandTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Phoenix\Tests\Command\CleanupCommand;
+
+use Phoenix\Tests\Command\MysqlCommandBehavior;
+
+class MysqlCleanupCommandTest extends CleanupCommandTest
+{
+    use MysqlCommandBehavior;
+}
