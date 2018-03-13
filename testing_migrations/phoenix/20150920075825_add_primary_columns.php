83783a8df020bd491ca710032f3c2356b80eda15
Michal Lulco <lulco@efabrica.sk>
2018-03-13 21:24:02 +0100

    Removed unused code

--- testing_migrations/phoenix/20150920075825_add_primary_columns.php
+++ testing_migrations/phoenix/20150920075825_add_primary_columns.php
@@ -4,7 +4,6 @@ namespace Phoenix\TestingMigrations;
 
 use Phoenix\Database\Element\Column;
 use Phoenix\Migration\AbstractMigration;
-use Ramsey\Uuid\Uuid;
 
 class AddPrimaryColumns extends AbstractMigration
 {
@@ -13,21 +12,10 @@ class AddPrimaryColumns extends AbstractMigration
         $this->table('table_3')
             ->addPrimaryColumns([new Column('id', 'integer', ['autoincrement' => true])])
             ->save();
-
-//        $this->table('table_4')
-//            ->addPrimaryColumns([new Column('identifier', 'uuid')], function (array $data) {
-//                $data['identifier'] = (string) Uuid::uuid4();
-//                return $data;
-//            })
-//            ->save();
     }
 
     public function down(): void
     {
-//        $this->table('table_4')
-//            ->dropColumn('identifier')
-//            ->save();
-
         $this->table('table_3')
             ->dropColumn('id')
             ->save();
