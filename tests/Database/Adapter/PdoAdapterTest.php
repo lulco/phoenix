6ff9bd8e6bf57c1cac57a501b6bf69289e5c9068
Michal Lulco <michal.lulco@gmail.com>
2018-03-17 21:22:16 +0100

    Dropped support for sqlite

--- tests/Database/Adapter/PdoAdapterTest.php
+++ tests/Database/Adapter/PdoAdapterTest.php
@@ -2,13 +2,11 @@
 
 namespace Phoenix\Tests\Database\Adapter;
 
-use DateTime;
 use InvalidArgumentException;
-use PDO;
-use PDOStatement;
-use Phoenix\Database\Adapter\SqliteAdapter;
 use Phoenix\Database\QueryBuilder\QueryBuilderInterface;
 use Phoenix\Exception\DatabaseQueryExecuteException;
+use Phoenix\Tests\Helpers\Adapter\MysqlCleanupAdapter;
+use Phoenix\Tests\Helpers\Pdo\MysqlPdo;
 use PHPUnit\Framework\TestCase;
 
 class PdoAdapterTest extends TestCase
@@ -17,18 +15,12 @@ class PdoAdapterTest extends TestCase
 
     protected function setUp()
     {
-        parent::setUp();
-        $pdo = new PDO('sqlite::memory:');
-        $this->adapter = new SqliteAdapter($pdo);
-    }
+        $pdo = new MysqlPdo();
+        $adapter = new MysqlCleanupAdapter($pdo);
+        $adapter->cleanupDatabase();
 
-    public function testSimple()
-    {
-        $this->assertInstanceOf(QueryBuilderInterface::class, $this->adapter->getQueryBuilder());
-
-        $this->assertInstanceOf(PDOStatement::class, $this->adapter->execute('CREATE TABLE "phoenix_test_table" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,"title" TEXT NOT NULL,"sorting" INTEGER NOT NULL);'));
-        $this->assertInstanceOf(PDOStatement::class, $this->adapter->execute('INSERT INTO "phoenix_test_table" VALUES (1, "first", 1);'));
-        $this->assertInstanceOf(PDOStatement::class, $this->adapter->execute('INSERT INTO "phoenix_test_table" VALUES (2, "second", 2);'));
+        $pdo = new MysqlPdo(getenv('PHOENIX_MYSQL_DATABASE'));
+        $this->adapter = new MysqlCleanupAdapter($pdo);
     }
 
     public function testTransaction()
@@ -36,69 +28,50 @@ class PdoAdapterTest extends TestCase
         $this->assertInstanceOf(QueryBuilderInterface::class, $this->adapter->getQueryBuilder());
 
         $this->adapter->startTransaction();
-        $this->adapter->execute('CREATE TABLE "phoenix_test_table" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,"title" TEXT NOT NULL,"sorting" INTEGER NOT NULL);');
-        $this->adapter->execute('INSERT INTO "phoenix_test_table" VALUES (1, "first", 1);');
-        $this->adapter->execute('INSERT INTO "phoenix_test_table" VALUES (2, "second", 2);');
+        $this->adapter->execute('CREATE TABLE `phoenix_test_table` (`id` int NOT NULL AUTO_INCREMENT,`title` varchar(255) NOT NULL,PRIMARY KEY (`id`));');
+        $this->adapter->execute('INSERT INTO `phoenix_test_table` VALUES (1, "first");');
+        $this->adapter->execute('INSERT INTO `phoenix_test_table` VALUES (2, "second");');
         $this->adapter->commit();
     }
 
     public function testInsert()
     {
-        $this->adapter->execute('CREATE TABLE "phoenix_test_table" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,"title" TEXT NOT NULL,"date" TEXT NOT NULL);');
-        $this->assertEquals(1, $this->adapter->insert('phoenix_test_table', ['id' => 1, 'title' => 'first', 'date' => new DateTime()]));
-        $this->assertEquals(2, $this->adapter->insert('phoenix_test_table', ['id' => 2, 'title' => 'second', 'date' => new DateTime()]));
+        $this->adapter->execute('CREATE TABLE `phoenix_test_table` (`id` int NOT NULL AUTO_INCREMENT,`title` varchar(255) NOT NULL,PRIMARY KEY (`id`));');
+        $this->assertEquals(1, $this->adapter->insert('phoenix_test_table', ['id' => 1, 'title' => 'first']));
+        $this->assertEquals(2, $this->adapter->insert('phoenix_test_table', ['id' => 2, 'title' => 'second']));
 
         $this->assertCount(2, $this->adapter->fetchAll('phoenix_test_table'));
-
-        $this->expectException(DatabaseQueryExecuteException::class);
-        $this->expectExceptionMessage('SQLSTATE[HY000]: no such table: phoenix_non_exist_test_table.', 1);
-        $this->adapter->insert('phoenix_non_exist_test_table', ['id' => 1, 'title' => 'first', 'date' => new DateTime()]);
     }
 
-    public function testInsertDataToNotExistColumn()
+    public function testInsertToNonExistingTable()
     {
-        $this->adapter->execute('CREATE TABLE "phoenix_test_table" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,"title" TEXT NOT NULL,"sorting" INTEGER NOT NULL);');
-
         $this->expectException(DatabaseQueryExecuteException::class);
-        $this->expectExceptionMessage('SQLSTATE[HY000]: table phoenix_test_table has no column named unknown.', 1);
-        $this->adapter->insert('phoenix_test_table', ['id' => 1, 'unknown' => 'first', 'sorting' => 1]);
+        $this->adapter->insert('phoenix_non_exist_test_table', ['id' => 1, 'title' => 'first']);
     }
 
     public function testMultiInsert()
     {
-        $this->adapter->execute('CREATE TABLE "phoenix_test_table" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,"title" TEXT NOT NULL,"date" TEXT NOT NULL);');
-        $this->assertEquals(2, $this->adapter->insert('phoenix_test_table', [['id' => 1, 'title' => 'first', 'date' => new DateTime()], ['id' => 2, 'title' => 'second', 'date' => new DateTime()]]));
+        $this->adapter->execute('CREATE TABLE `phoenix_test_table` (`id` int NOT NULL AUTO_INCREMENT,`title` varchar(255) NOT NULL,PRIMARY KEY (`id`));');
+        $this->assertEquals(2, $this->adapter->insert('phoenix_test_table', [['id' => 1, 'title' => 'first'], ['id' => 2, 'title' => 'second']]));
         $this->assertCount(2, $this->adapter->fetchAll('phoenix_test_table'));
-    }
-
-    public function testThrowingException()
-    {
-        $this->assertInstanceOf(QueryBuilderInterface::class, $this->adapter->getQueryBuilder());
-
-        $this->adapter->execute('CREATE TABLE "phoenix_test_table" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,"title" TEXT NOT NULL,"sorting" INTEGER NOT NULL);');
-        $this->expectException(DatabaseQueryExecuteException::class);
-        $this->expectExceptionMessage('SQLSTATE[HY000]: table "phoenix_test_table" already exists. Query CREATE TABLE "phoenix_test_table" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,"title" TEXT NOT NULL,"sorting" INTEGER NOT NULL); fails', 1);
-        $this->adapter->execute('CREATE TABLE "phoenix_test_table" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,"title" TEXT NOT NULL,"sorting" INTEGER NOT NULL);');
     }
 
     public function testUpdate()
     {
-        $this->assertInstanceOf(QueryBuilderInterface::class, $this->adapter->getQueryBuilder());
-
-        $this->adapter->execute('CREATE TABLE "phoenix_test_table" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,"title" TEXT NOT NULL,"date" TEXT NOT NULL);');
-        $this->assertEquals(1, $this->adapter->insert('phoenix_test_table', ['id' => 1, 'title' => 'first', 'date' => new DateTime()]));
+        $this->adapter->execute('CREATE TABLE `phoenix_test_table` (`id` int NOT NULL AUTO_INCREMENT,`title` varchar(255) NOT NULL,PRIMARY KEY (`id`));');
+        $this->assertEquals(1, $this->adapter->insert('phoenix_test_table', ['id' => 1, 'title' => 'first']));
         $item = $this->adapter->fetch('phoenix_test_table', ['title'], ['id' => 1]);
         $this->assertEquals('first', $item['title']);
 
-        $this->assertTrue($this->adapter->update('phoenix_test_table', ['id' => 1, 'title' => 'second', 'date' => new DateTime()], ['id' => 1]));
+        $this->assertTrue($this->adapter->update('phoenix_test_table', ['id' => 1, 'title' => 'second'], ['id' => 1]));
         $item = $this->adapter->fetch('phoenix_test_table', ['title'], ['id' => 1], ['title']);
         $this->assertEquals('second', $item['title']);
 
-        $this->assertTrue($this->adapter->update('phoenix_test_table', ['id' => 1, 'title' => 'third', 'date' => new DateTime()], [], 'id = 1'));
+        $this->assertTrue($this->adapter->update('phoenix_test_table', ['id' => 1, 'title' => 'third'], [], 'id = 1'));
         $items = $this->adapter->fetchAll('phoenix_test_table', ['title'], ['id' => 1], null, ['title' => 'DESC'], ['id']);
         $this->assertEquals('third', $items[0]['title']);
 
-        $this->assertEquals(2, $this->adapter->insert('phoenix_test_table', ['id' => 2, 'title' => 'fourth', 'date' => new DateTime()]));
+        $this->assertEquals(2, $this->adapter->insert('phoenix_test_table', ['id' => 2, 'title' => 'fourth']));
         $this->assertTrue($this->adapter->update('phoenix_test_table', ['title' => 'multi update'], ['id' => [1, 2]]));
 
         $items = $this->adapter->fetchAll('phoenix_test_table', ['title'], ['id' => [1, 2]]);
@@ -106,61 +79,53 @@ class PdoAdapterTest extends TestCase
         foreach ($items as $item) {
             $this->assertEquals('multi update', $item['title']);
         }
+    }
 
+    public function testUpdateItemInNonExistingTable()
+    {
         $this->expectException(DatabaseQueryExecuteException::class);
-        $this->expectExceptionMessage('SQLSTATE[HY000]: no such table: phoenix_non_exist_test_table.', 1);
-        $this->adapter->update('phoenix_non_exist_test_table', ['id' => 1, 'title' => 'first', 'date' => new DateTime()]);
+        $this->adapter->update('phoenix_non_exist_test_table', ['id' => 1, 'title' => 'first']);
     }
 
     public function testSelect()
     {
-        $this->assertInstanceOf(QueryBuilderInterface::class, $this->adapter->getQueryBuilder());
-
-        $this->adapter->execute('CREATE TABLE "phoenix_test_table" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,"title" TEXT NOT NULL,"date" TEXT NOT NULL);');
-        $this->assertEquals(1, $this->adapter->insert('phoenix_test_table', ['id' => 1, 'title' => 'first', 'date' => new DateTime()]));
-        $this->assertEquals(2, $this->adapter->insert('phoenix_test_table', ['id' => 2, 'title' => 'second', 'date' => new DateTime()]));
+        $this->adapter->execute('CREATE TABLE `phoenix_test_table` (`id` int NOT NULL AUTO_INCREMENT,`title` varchar(255) NOT NULL,PRIMARY KEY (`id`));');
+        $this->assertEquals(1, $this->adapter->insert('phoenix_test_table', ['id' => 1, 'title' => 'first']));
+        $this->assertEquals(2, $this->adapter->insert('phoenix_test_table', ['id' => 2, 'title' => 'second']));
 
         $items = $this->adapter->select('SELECT * FROM `phoenix_test_table`');
         $this->assertCount(2, $items);
         foreach ($items as $item) {
-            $this->assertCount(3, $item);
+            $this->assertCount(2, $item);
             $this->assertArrayHasKey('id', $item);
             $this->assertArrayHasKey('title', $item);
-            $this->assertArrayHasKey('date', $item);
         }
 
         $this->expectException(InvalidArgumentException::class);
         $this->expectExceptionMessage('Only select query can be executed in select method');
-        $this->adapter->select('INSERT INTO `phoenix_test_table` (`id`, `title`, `date`) VALUES (3, "third", ' . (new DateTime)->format('Y-m-d H:i:s') . ')');
+        $this->adapter->select('INSERT INTO `phoenix_test_table` (`id`, `title`) VALUES (3, "third")');
     }
 
-    public function testFetch()
+    public function testFetchAll()
     {
-        $this->assertInstanceOf(QueryBuilderInterface::class, $this->adapter->getQueryBuilder());
-
-        $this->adapter->execute('CREATE TABLE "phoenix_test_table" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,"title" TEXT NOT NULL,"date" TEXT NOT NULL);');
-        $this->assertEquals(1, $this->adapter->insert('phoenix_test_table', ['id' => 1, 'title' => 'first', 'date' => new DateTime()]));
-        $this->assertEquals(2, $this->adapter->insert('phoenix_test_table', ['id' => 2, 'title' => 'second', 'date' => new DateTime()]));
+        $this->adapter->execute('CREATE TABLE `phoenix_test_table` (`id` int NOT NULL AUTO_INCREMENT,`title` varchar(255) NOT NULL,PRIMARY KEY (`id`));');
+        $this->assertEquals(1, $this->adapter->insert('phoenix_test_table', ['id' => 1, 'title' => 'first']));
+        $this->assertEquals(2, $this->adapter->insert('phoenix_test_table', ['id' => 2, 'title' => 'second']));
 
         $items = $this->adapter->fetchAll('phoenix_test_table');
         $this->assertCount(2, $items);
         foreach ($items as $item) {
-            $this->assertCount(3, $item);
+            $this->assertCount(2, $item);
             $this->assertArrayHasKey('id', $item);
             $this->assertArrayHasKey('title', $item);
-            $this->assertArrayHasKey('date', $item);
         }
-
-        $this->expectException(DatabaseQueryExecuteException::class);
-        $this->expectExceptionMessage('SQLSTATE[HY000]: no such table: phoenix_non_exist_test_table.');
-        $this->adapter->fetchAll('phoenix_non_exist_test_table');
     }
 
     public function testDelete()
     {
-        $this->adapter->execute('CREATE TABLE "phoenix_test_table" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,"title" TEXT NOT NULL,"date" TEXT NOT NULL);');
-        $this->assertEquals(1, $this->adapter->insert('phoenix_test_table', ['id' => 1, 'title' => 'first', 'date' => new DateTime()]));
-        $this->assertEquals(2, $this->adapter->insert('phoenix_test_table', ['id' => 2, 'title' => 'second', 'date' => new DateTime()]));
+        $this->adapter->execute('CREATE TABLE `phoenix_test_table` (`id` int NOT NULL AUTO_INCREMENT,`title` varchar(255) NOT NULL,PRIMARY KEY (`id`));');
+        $this->assertEquals(1, $this->adapter->insert('phoenix_test_table', ['id' => 1, 'title' => 'first']));
+        $this->assertEquals(2, $this->adapter->insert('phoenix_test_table', ['id' => 2, 'title' => 'second']));
 
         $items = $this->adapter->fetchAll('phoenix_test_table');
         $this->assertCount(2, $items);
@@ -169,8 +134,8 @@ class PdoAdapterTest extends TestCase
         $items = $this->adapter->fetchAll('phoenix_test_table');
         $this->assertCount(0, $items);
 
-        $this->assertEquals(1, $this->adapter->insert('phoenix_test_table', ['id' => 1, 'title' => 'first', 'date' => new DateTime()]));
-        $this->assertEquals(2, $this->adapter->insert('phoenix_test_table', ['id' => 2, 'title' => 'second', 'date' => new DateTime()]));
+        $this->assertEquals(1, $this->adapter->insert('phoenix_test_table', ['id' => 1, 'title' => 'first']));
+        $this->assertEquals(2, $this->adapter->insert('phoenix_test_table', ['id' => 2, 'title' => 'second']));
         $items = $this->adapter->fetchAll('phoenix_test_table');
         $this->assertCount(2, $items);
 
@@ -178,20 +143,22 @@ class PdoAdapterTest extends TestCase
         $items = $this->adapter->fetchAll('phoenix_test_table');
         $this->assertCount(1, $items);
 
-        $this->assertEquals(2, $this->adapter->insert('phoenix_test_table', ['id' => 2, 'title' => 'second', 'date' => new DateTime()]));
+        $this->assertEquals(2, $this->adapter->insert('phoenix_test_table', ['id' => 2, 'title' => 'second']));
         $this->adapter->delete('phoenix_test_table', [], 'id < 3');
         $items = $this->adapter->fetchAll('phoenix_test_table');
         $this->assertCount(0, $items);
 
-        $this->assertEquals(1, $this->adapter->insert('phoenix_test_table', ['id' => 1, 'title' => 'first', 'date' => new DateTime()]));
-        $this->assertEquals(2, $this->adapter->insert('phoenix_test_table', ['id' => 2, 'title' => 'second', 'date' => new DateTime()]));
+        $this->assertEquals(1, $this->adapter->insert('phoenix_test_table', ['id' => 1, 'title' => 'first']));
+        $this->assertEquals(2, $this->adapter->insert('phoenix_test_table', ['id' => 2, 'title' => 'second']));
         $items = $this->adapter->fetchAll('phoenix_test_table');
         $this->assertCount(2, $items);
 
         $this->adapter->delete('phoenix_test_table', ['id' => [1, 2]]);
+    }
 
+    public function testDeleteFromNonExistingTable()
+    {
         $this->expectException(DatabaseQueryExecuteException::class);
-        $this->expectExceptionMessage('SQLSTATE[HY000]: no such table: phoenix_non_exist_test_table.', 1);
         $this->adapter->delete('phoenix_non_exist_test_table');
     }
 
@@ -201,12 +168,12 @@ class PdoAdapterTest extends TestCase
 
         $this->adapter->startTransaction();
         try {
-            $this->adapter->execute('CREATE TABLE "phoenix_test_table" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,"title" TEXT NOT NULL,"sorting" INTEGER NOT NULL);');
-            $this->adapter->execute('CREATE TABLE "phoenix_test_table" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,"title" TEXT NOT NULL,"sorting" INTEGER NOT NULL);');
+            $this->adapter->execute('CREATE TABLE `phoenix_test_table` (`id` int NOT NULL AUTO_INCREMENT,`title` varchar(255) NOT NULL,PRIMARY KEY (`id`));');
+            $this->adapter->execute('CREATE TABLE `phoenix_test_table` (`id` int NOT NULL AUTO_INCREMENT,`title` varchar(255) NOT NULL,PRIMARY KEY (`id`));');
             $this->adapter->commit();
         } catch (DatabaseQueryExecuteException $e) {
-            $this->assertEquals(1, $e->getCode());
-            $this->assertEquals('SQLSTATE[HY000]: table "phoenix_test_table" already exists. Query CREATE TABLE "phoenix_test_table" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,"title" TEXT NOT NULL,"sorting" INTEGER NOT NULL); fails', $e->getMessage());
+            $this->assertEquals(1050, $e->getCode());
+            $this->assertEquals('SQLSTATE[42S01]: Table \'phoenix_test_table\' already exists. Query CREATE TABLE `phoenix_test_table` (`id` int NOT NULL AUTO_INCREMENT,`title` varchar(255) NOT NULL,PRIMARY KEY (`id`)); fails', $e->getMessage());
             $this->adapter->rollback();
         }
     }
