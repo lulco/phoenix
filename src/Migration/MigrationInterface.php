566d4285c365f343344a8a1576c143c7df79f240
Raimondas Rimkevičius <github@mekdrop.name>
2020-11-25 22:08:23 +0200

    Added some comments to MigrationInterface

--- src/Migration/MigrationInterface.php
+++ src/Migration/MigrationInterface.php
@@ -4,19 +4,63 @@ namespace Phoenix\Migration;
 
 use Phoenix\Database\Element\Structure;
 
+/**
+ * Every migration must implement this interface
+ *
+ * @package Phoenix\Migration
+ */
 interface MigrationInterface
 {
+    /**
+     * Gets date-time for migration
+     *
+     * @return string
+     */
     public function getDatetime(): string;
 
+    /**
+     * Gets class name without extra \
+     *
+     * @return string
+     */
     public function getClassName(): string;
 
+    /**
+     * Gets class name with all characters
+     *
+     * @return string
+     */
     public function getFullClassName(): string;
 
+    /**
+     * Execute this migration to never version
+     *
+     * @param bool $dry Run migration as dry?
+     *
+     * @return array
+     */
     public function migrate(bool $dry = false): array;
 
+    /**
+     * Execute this migration to older version?
+     *
+     * @param bool $dry Run migration as dry?
+     *
+     * @return array
+     */
     public function rollback(bool $dry = false): array;
 
+    /**
+     * Updates table structure
+     *
+     * @param Structure $structure
+     */
     public function updateStructure(Structure $structure): void;
 
+    /**
+     * Gets executed queries
+     *
+     * @return array
+     */
     public function getExecutedQueries(): array;
 }
