dd98277899d646a323eaa7e96c1a55d162adb7c1
Michal Lulco <lulco@efabrica.sk>
2021-01-15 16:30:37 +0100

    Fixed sniffer and added check if tableStructure exists when altering table

--- src/Database/QueryBuilder/MysqlQueryBuilder.php
+++ src/Database/QueryBuilder/MysqlQueryBuilder.php
@@ -108,11 +108,10 @@ class MysqlQueryBuilder extends CommonQueryBuilder implements QueryBuilderInterf
         $queries = $this->dropIndexes($table);
 
         $tableStructure = $this->adapter->getStructure()->getTable($table->getName());
-        if (($table->getCharset() && $table->getCharset() !== $tableStructure->getCharset()) || ($table->getCollation() && $table->getCollation() !== $tableStructure->getCollation())) {
+        if ($tableStructure && (($table->getCharset() && $table->getCharset() !== $tableStructure->getCharset()) || ($table->getCollation() && $table->getCollation() !== $tableStructure->getCollation()))) {
             $queries[] = 'ALTER TABLE ' . $this->escapeString($table->getName()) . $this->createCharset($table->getCharset(), $table->getCollation()) . ';';
         }
         if ($table->getColumnsToRename()) {
-
             $query = 'ALTER TABLE ' . $this->escapeString($table->getName()) . ' ';
             $columnList = [];
             foreach ($table->getColumnsToRename() as $oldName => $newName) {
