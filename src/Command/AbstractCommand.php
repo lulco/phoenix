6009fb975c8d117b19f8e368c888782cf8067ad5
Michal Lulco <lulco@efabrica.sk>
2021-01-18 13:54:35 +0100

    Zmeneny command pre syntax checker

--- src/Command/AbstractCommand.php
+++ src/Command/AbstractCommand.php
@@ -17,7 +17,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 abstract class AbstractCommand extends Command
 {
-    protected Config $config = null;
+    protected ?Config $config = null;
 
     /** @var AdapterInterface */
     protected $adapter;
