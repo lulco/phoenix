f3a0f4543a33f48b6025a2850d737f3b81e5af7f
Michal Lulco <lulco@efabrica.sk>
2021-08-23 11:56:51 +0200

    Removed phpstan php 8.x special conifg

--- phpstan-php8x.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$config = [];
-if (PHP_VERSION_ID >= 80000) {
-//    $config['parameters']['ignoreErrors'][] = '#no value type specified in iterable type PDOStatement#';
-}
-
-return $config;
