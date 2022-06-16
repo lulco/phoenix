da79f7ef01ee77c3d45ae0d541abcb1765380ccc
Marcel Vrana <marcel.vrana@gmail.com>
2022-06-16 20:26:19 +0200

    Code fix

--- src/Migration/MigrationCreator.php
+++ src/Migration/MigrationCreator.php
@@ -21,8 +21,12 @@ final class MigrationCreator
     public function create(string $up, string $down, string $migrationDir): string
     {
         $template = $this->templateManager->createMigrationFromTemplate($up, $down);
-        if(!is_dir($migrationDir)) mkdir($migrationDir, 0777, true);
-        if(!is_writable($migrationDir)) chmod($migrationDir, 0777);
+        if (!is_dir($migrationDir)) {
+            mkdir($migrationDir, 0777, true);
+        }
+        if (!is_writable($migrationDir)) {
+            chmod($migrationDir, 0777);
+        }
         $migrationPath = $migrationDir . '/' . $this->migrationNameCreator->getFileName();
         file_put_contents($migrationPath, $template);
         return (string)realpath($migrationPath);
